0f3a42d169e7bc0537c8f3cbbdec156b9f41f7fb
David lowe <mrdavidhlowe@gmail.com>
2023-08-22 15:09:49 +0100

    booking controller new and create complete

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.shoe_id = params[:id]
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
