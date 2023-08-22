c8ee8c706b7b9cd4f2fb6fe2dc029e63345f36e1
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-22 13:22:30 +0100

    shoes index page is fully complete and is_rented was added to shoes db

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,6 +1,8 @@
 class ShoesController < ApplicationController
   def index
-    @shoes = Shoe.all
+    # @shoes will be the shoes that the user can rent out which have not been rented out.
+    @shoes = Shoe.where(is_rented: false).where.not(user_id: current_user.id)
+    # @my_shoes are the shoes that the user will have listed to be rented out.
     @my_shoes = Shoe.where("user_id = #{current_user.id}")
   end
 end
