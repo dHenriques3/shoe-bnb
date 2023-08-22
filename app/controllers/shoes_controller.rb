8634cea57ae758a894f29414b6367df49a470e1b
Nii Addo eagleaddo@yahoo.co.uk <eagleaddo@yahoo.co.uk>
2023-08-22 15:24:41 +0100

    shoes controller and view files done

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,5 +1,26 @@
 class ShoesController < ApplicationController
   def index
-    # @shoes = Shoes.all
+    @shoes = Shoes.all
+  end
+
+  def new
+    @shoe = Shoe.new
+  end
+
+  def create
+    @shoe = Shoe.new(shoe_params)
+    @shoe.user = current_user
+    raise
+    if @shoe.save
+      redirect_to shoes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shoe_params
+    params.require(:shoe).permit(:name, :brand, :price_per_day)
   end
 end
